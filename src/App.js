29252210e2a1a5e1e3fe68aa9a2c300e89566a8b
Igorplisko <igorplisko95@gmail.com>
2021-11-29 17:36:36 +0200

    innit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="wrapper">
+      <header>
 
+        <div className="headerLeft">
+          <swg />
 
+          <div className="headerInfo">
+            <h3> React Sneakers </h3>
+            <p>Shop of the best sneakers</p>
+          </div>
+
+          <ul>
+            <li>
+              <swg />
+              <span>700 $</span>
+            </li>
+            <li>
+              <swg />
+            </li>
+          </ul>
+        </div>
       </header>
+
+      <div className="content">
+        <h1>All sneakers</h1>
+        .............
+        content
+      </div>
+
     </div>
   );
 }
