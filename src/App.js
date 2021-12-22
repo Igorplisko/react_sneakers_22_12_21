2b1a691eb50464bdbf1d81c32176af36fb585caa
igorplisko <igorplisko95@gmail.com>
2021-12-22 20:42:11 +0200

    react sneakers#2 video time 1:50hr

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './components/Card';
+import Drawer from './components/Drawer';
 import Header from './components/Header';
 // import { Link } from 'react-router-dom';
 
@@ -7,74 +8,11 @@ function App() {
   return (
     <div className="wrapper clear">
 
+      {/* <div className="overlay"> */}
       <div style={{ display: 'none' }} className="overlay">
-        <div className="drawer d-flex ">
-          <h2 className="mb-20 d-flex justify-between cu-p">Корзина
-            <img
-              // onClick={() => onRemove(obj.id)}
-              className="removeBtn"
-              src="img/btn-remove.svg"
-              alt="Remove"
-            />
 
-          </h2>
-
-
-          <div className="cartItem d-flex align-center mb-20">
-            {/* <img
-              className='mr-15'
-              width={70}
-              height={70}
-              src="/img/sneakers/1.jpg"
-              alt=""
-            /> */}
-
-            <div style={{ backgroundImage: `url("/img/sneakers/1.jpg")` }}
-              className="cartItemImg"></div>
-
-            <div className="mr-2">
-              <p className="mb-5"> Мужские кросовки NIke Air MAx 270</p>
-              <b>500 $</b>
-            </div>
-            <img
-              // onClick={() => onRemove(obj.id)}
-              className="removeBtn"
-              src="img/btn-remove.svg"
-              alt="Remove"
-            />
-
-
-          </div>
-
-
-          <div className='items'>
-            <div className="cartTotalBlock">
-              <ul >
-                <li >
-                  <span>Итого: </span>
-                  <div></div>
-                  <b>800$</b>
-                </li>
-                <li >
-                  <span> Налог 5%:</span>
-                  <div></div>
-                  <b>40$ </b>
-                </li>
-              </ul>
-              <button className="greenButton" >
-                Оформить заказ
-                <img src="/img/arrow.svg" alt="Arrow" />
-              </button>
-
-            </div>
-
-
-
-          </div>
-
-        </div>
+        <Drawer />
       </div>
-
       <Header />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40 ">
