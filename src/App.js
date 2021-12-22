9e75be58976d48d96ba16531234cb3ac1d2bcfc5
igorplisko <igorplisko95@gmail.com>
2021-12-22 20:31:53 +0200

    innit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './components/Card';
+import Header from './components/Header';
 // import { Link } from 'react-router-dom';
 
 function App() {
@@ -74,43 +75,8 @@ function App() {
         </div>
       </div>
 
-
-
-
-
-
-      <header className="d-flex justify-between align-center p-40">
-        {/* <Link to="/"> */}
-        <div className="d-flex align-center">
-          <img width={40} height={40} src="img/logo.png" alt="Logotype" />
-          <div>
-            <h3 className="text-uppercase">React Sneakers</h3>
-            <p className="opacity-5">Магазин лучших кроссовок</p>
-          </div>
-        </div>
-        {/* </Link> */}
-        <ul className="d-flex">
-          <li className="mr-30 cu-p">
-            <img width={18} height={18} src="img/cart.svg" alt="Корзина" />
-            <span> 1200 грн.</span>
-          </li>
-          <li className="mr-20 cu-p">
-            {/* <Link to="/favorites"> */}
-            <img width={18} height={18} src="img/heart.svg" alt="Закладки" />
-            {/* </Link> */}
-          </li>
-          <li>
-            {/* {/* <Link to="/orders"> */}
-            <img width={18} height={18} src="img/user.svg" alt="Пользователь" />
-            {/* </Link> */}
-          </li>
-        </ul>
-      </header>
-
-
+      <Header />
       <div className="content p-40">
-
-
         <div className="d-flex align-center justify-between mb-40 ">
           <h1 >All sneakers</h1>
           <div className="search-block d-flex">
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Header() {
+   return (
+      <header className="d-flex justify-between align-center p-40">
+         {/* <Link to="/"> */}
+         <div className="d-flex align-center">
+            <img width={40} height={40} src="img/logo.png" alt="Logotype" />
+            <div>
+               <h3 className="text-uppercase">React Sneakers</h3>
+               <p className="opacity-5">Магазин лучших кроссовок</p>
+            </div>
+         </div>
+         {/* </Link> */}
+         <ul className="d-flex">
+            <li className="mr-30 cu-p">
+               <img width={18} height={18} src="img/cart.svg" alt="Корзина" />
+               <span> 1200 грн.</span>
+            </li>
+            <li className="mr-20 cu-p">
+               {/* <Link to="/favorites"> */}
+               <img width={18} height={18} src="img/heart.svg" alt="Закладки" />
+               {/* </Link> */}
+            </li>
+            <li>
+               {/* {/* <Link to="/orders"> */}
+               <img width={18} height={18} src="img/user.svg" alt="Пользователь" />
+               {/* </Link> */}
+            </li>
+         </ul>
+      </header>
+
+   )
+}
+
+export default Header
