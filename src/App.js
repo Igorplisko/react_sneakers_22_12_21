0f34c8537a73bdf37ed59dd159336a84cb22565f
igorplisko <igorplisko95@gmail.com>
2021-12-22 20:17:42 +0200

    innit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Card from './components/Card';
 // import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="wrapper clear">
 
-      <div className="overlay">
+      <div style={{ display: 'none' }} className="overlay">
         <div className="drawer d-flex ">
           <h2 className="mb-20 d-flex justify-between cu-p">Корзина
             <img
@@ -120,43 +121,9 @@ function App() {
         </div>
 
         <div className="d-flex">
+          <Card />
+          <Card />
 
-          <div className="card">
-            <div className="favorite">
-              <img src="img/unliked.svg" alt="Unliked" />
-            </div>
-
-            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
-
-            <h5> Мужские кросовки NIke Blue</h5>
-            <div className="d-flex justify-between align-center">
-
-              <div className="d-flex flex-column ">
-                <span> Price:</span>
-                <b>10 999грн.</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="" />
-              </button>
-            </div>
-          </div>
-
-
-          <div className="card">
-            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
-
-            <h5> Мужские кросовки NIke Blue</h5>
-            <div className="d-flex justify-between align-center">
-
-              <div className="d-flex flex-column ">
-                <span> Price:</span>
-                <b>10 999грн.</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="" />
-              </button>
-            </div>
-          </div>
 
 
 
